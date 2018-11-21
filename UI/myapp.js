50ce55fbfb6955126c0c51a8543701296742d416
valerie <ronovalerie@gmail.com>
2018-11-22 01:32:31 +0300

    create and modify javaScript file

---
+++ UI/myapp.js
@@ -0,0 +1,13 @@
+$('#container3').hide();
+
+$('#sign_in').on('click', function(){
+$('#container2').hide();
+$('#container3').show();
+
+});
+
+$('#sign_up').on('click', function(){
+$('#container3').hide();
+$('#container2').show();
+
+});
