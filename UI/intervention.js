7db8540e89725c57238940b5d4262bade7dff412
valerie <ronovalerie@gmail.com>
2019-01-25 11:38:02 +0300

    enable upload to firebase

--- UI/intervention.js
+++ UI/intervention.js
@@ -1,5 +1,45 @@
 // create incident fetch method
+let user2 = document.getElementById('user_name').innerText;
 document.getElementById('incidentForm').addEventListener('submit', createIntervention)
+
+let files2;
+function handleFileSelect2(event){
+    var files2 = event.target.files;
+    // loop through the FileList and render the images as thumbnails
+    for (var i=0, f; f = files2[i]; i++){
+        var reader2 = new FileReader();
+        
+        // img.src = URL.createObjectURL(this.files[0]);
+
+        // capture file information
+        reader2.onload = (function(theInterventionFile){
+            return function(e){
+                var span = document.createElement('span');
+                span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theInterventionFile.name), '"/>'].join('');
+                var x2 = document.getElementById(event.target.id).nextElementSibling;
+                x2.insertBefore(span, null);
+                console.log(theInterventionFile)
+                // create a child directory called images, and place the file inside this directory
+                const uploadTask2 = storageRef.child(`${user[i]}/${theInterventionFile.name}`).put(theInterventionFile); 
+                uploadTask2.on('state_changed', (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                }, (error) => {
+                    // Handle unsuccessful uploads
+                    console.log(error);
+                }, () => {
+                    // Do something once upload is complete
+                    console.log('success');
+                });
+            }
+        })(f);
+
+        reader2.readAsDataURL(f);
+      }
+    };
+
+document.getElementById('imagesIntervention').addEventListener('change', handleFileSelect2, false);
+document.getElementById('videosIntervention').addEventListener('change', handleFileSelect2, false);
 
 function createIntervention(event){
     event.preventDefault();
@@ -31,3 +71,20 @@ function createIntervention(event){
                 console.log(data)
             })       
 }
+
+var intervention = document.getElementById("add_location2");
+
+function addLocation2() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition2);
+    } else { 
+        intervention.value = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition2(position) {
+    intervention.value = position.coords.latitude +
+    ", " + position.coords.longitude;
+}
+
+intervention.addEventListener("click", addLocation2, false);
--- UI/redflag.js
+++ UI/redflag.js
@@ -1,5 +1,47 @@
 // create incident fetch method
+let user = document.getElementById('user_name').innerText;
 document.getElementById('redFlagForm').addEventListener('submit', createRedflag)
+
+let files;
+function handleFileSelect(event){
+    var files = event.target.files;
+    // loop through the FileList and render the images as thumbnails
+    for (var i=0, f; f = files[i]; i++){
+        var reader = new FileReader();
+        
+        // img.src = URL.createObjectURL(this.files[0]);
+
+        // capture file information
+        reader.onload = (function(theFile){
+            return function(e){
+                var span = document.createElement('span');
+                span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+                var x = document.getElementById(event.target.id).nextElementSibling;
+                x.insertBefore(span, null);
+                console.log(theFile)
+                // create a child directory called images, and place the file inside this directory
+                const uploadTask = storageRef.child(`${user[i]}/${theFile.name}`).put(theFile); 
+                uploadTask.on('state_changed', (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                }, (error) => {
+                    // Handle unsuccessful uploads
+                    console.log(error);
+                }, () => {
+                    // Do something once upload is complete
+                    console.log('success');
+                });
+            }
+        })(f);
+
+        reader.readAsDataURL(f);
+      }
+    };
+
+      
+document.getElementById('images').addEventListener('change', handleFileSelect, false);
+document.getElementById('videos').addEventListener('change', handleFileSelect, false);
+
     
 function createRedflag(event){
     event.preventDefault();
@@ -35,6 +77,23 @@ function createRedflag(event){
                     window.alert(JSON.stringify(data['message']))
                 }
             })
-    
-        
 }
+
+var redFlag = document.getElementById("add_location");
+
+function addLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        redFlag.value = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    redFlag.value = position.coords.latitude +
+    ", " + position.coords.longitude;
+}
+
+redFlag.addEventListener("click", addLocation, false);
+
+
