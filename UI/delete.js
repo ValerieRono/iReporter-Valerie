f9a06548cd7e2e2d33474d62669c982a8f8dd875
valerie <ronovalerie@gmail.com>
2019-02-05 12:40:46 +0300

    fis delete.js codacy issues

--- UI/delete.js
+++ UI/delete.js
@@ -1,32 +1,32 @@
 // delete record fetch method
-let records = document.getElementById('past_records');
+let records = document.getElementById("past_records");
 
 records.addEventListener("click", function(event) {
 	// event.target is the clicked element!
 	// If it was a button
-	if (event.target.className != 'delete_record_btn') return;
+	if (event.target.className !== "delete_record_btn") {return};
 
     //let button = event.target.closest('.individual_record');
     event.preventDefault();
 
     fetch(`https://ireporter-valerie.herokuapp.com/api/v2/incidents/${event.target.id}`, {
-                method: 'DELETE',
+                method: "DELETE",
                 headers: {
-                    'Authorization': bearer,
-                    'Accept':'application/json',
-                    'Content-Type': 'application/json',
-                    'mode':'cors'
+                    "Authorization": bearer,
+                    "Accept":"application/json",
+                    "Content-Type": "application/json",
+                    "mode":"cors"
                 }
             })
             .then((response) => response.json())
             .then((data) => {
                 if(data.status === 200){
-                    console.log(data)
-                    window.alert(JSON.stringify(data['data'][0]['message']))
-                    window.location.replace('profile.html')
+                    console.log(data);
+                    window.alert(JSON.stringify(data["data"][0]["message"]));
+                    window.location.replace("profile.html");
                 }else{
                     console.log(data)
-                    window.alert(JSON.stringify(data['message']))
+                    window.alert(JSON.stringify(data["message"]))
                 }
             })
 		
